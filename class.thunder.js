531041b2b534f4c40f173de7204bc3387c5d82b4
Davit <davit.musheghyan.y@tumo.org>
2018-11-21 23:08:28 +0400

    thunder

--- class.thunder.js
+++ class.thunder.js
@@ -1,8 +1,26 @@
-class thunder
-{
+class thunder{
     constructor(x, y) {
         super(x,y)
         this.MIN_energy = 2;
         this.energy = 5;
+    }
+
+    getNewCoordinates() {
+        this.directions = [
+            [this.x - 1, this.y - 1],
+            [this.x, this.y - 1],
+            [this.x + 1, this.y - 1],
+            [this.x - 1, this.y],
+            [this.x + 1, this.y],
+            [this.x - 1, this.y + 1],
+            [this.x, this.y + 1],
+            [this.x + 1, this.y + 1]
+        ];
+    }
+
+    hit(){
+        
+
+
     }
 }
